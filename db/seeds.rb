1f27a7bebe3c2221e1a08022096c4c69499b8296
olartem <olartem03@gmail.com>
2021-11-25 16:38:52 +0200

    Seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(first_name: 'first name', last_name: 'last name', birth_date: DateTime.now, gender: 'Admin',
-            email: 'admin@admin.com', password: 'admin1', is_admin: true)
+User.create!(first_name: 'first name', last_name: 'last name', birth_date: DateTime.now, gender: 'Admin',
+            email: 'admin@admin.com', password: 'admin1', is_admin: true, city: 'city')
