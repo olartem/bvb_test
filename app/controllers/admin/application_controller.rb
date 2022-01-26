f0e95560a0cb8f41a728969843c32ed944a6d16f
olartem <olartem03@gmail.com>
2022-01-26 14:28:32 +0200

    administrate i18n fix

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,11 +7,28 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
+    before_action :set_locale
 
     def authenticate_admin
       authenticate_user!
       redirect_to home_index_path, alert: 'You are not an admin' unless current_user.is_admin?
     end
+
+    private
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
+    def set_locale
+      I18n.locale = extract_locale || I18n.default_locale
+    end
+  
+    def extract_locale
+      parsed_locale = params[:locale]
+      I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+    end
+    
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    
+    namespace :admin do
+      resources :users do
+        delete :avatar, on: :member, action: :destroy_avatar
+      end
+      resources :donations
+      resources :projects do
+        delete :images, on: :member, action: :destroy_avatar
+      end
 
+      root to: 'users#index'
+    end
     
     root 'home#index'
     devise_for :users, skip: :omniauth_callbacks, controllers: { registrations: 'users/registrations' }
@@ -13,15 +22,5 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users, skip: [:session, :password, :registration], controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  namespace :admin do
-    resources :users do
-      delete :avatar, on: :member, action: :destroy_avatar
-    end
-    resources :donations
-    resources :projects do
-      delete :images, on: :member, action: :destroy_avatar
-    end
-
-    root to: 'users#index'
-  end
+  
 end
