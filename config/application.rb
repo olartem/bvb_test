b92fe18e968c3fb8c68ef22179888c9b85cf82df
olartem <olartem03@gmail.com>
2022-01-28 09:23:49 +0200

    Ukraine i18n done

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Charity
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.available_locales = [:en, :ru]
+    config.i18n.available_locales = [:en, :ru, :uk]
     config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
