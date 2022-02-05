b2e0b008943bd077ddabcd6437fbb46678108291
olartem <olartem03@gmail.com>
2022-02-05 11:48:09 +0200

    map fix, root filters done, progress bar done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
     
   def index
+    @projects = Project.where(["is_deleted = ? and end_date > ?", "false", DateTime.now]).order(end_date: :desc)
+    if params[:sort] == 'all'
+      @projects = Project.where(["is_deleted = ?", "false"]).order(end_date: :desc)
+    end
+    if params[:sort] == 'finished'
+      @projects = Project.where(["is_deleted = ? and end_date < ?", "false", DateTime.now]).order(end_date: :desc)
+    end
   end
 end
