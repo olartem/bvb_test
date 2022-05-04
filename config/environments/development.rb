4b9a55f73c66c57d6ec0dfaa4f1362b46689d7f2
olartem <olartem03@gmail.com>
2022-05-05 00:29:42 +0300

    metatags v2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.hosts << "https://bfcd-62-122-207-195.eu.ngrok.io"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
